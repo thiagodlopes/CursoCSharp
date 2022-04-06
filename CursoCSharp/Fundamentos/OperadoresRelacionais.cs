ec9fb96eb4c1ad7178466661d31a110960eec261
Thiago Lopes <thiagodaniellopes99@gmail.com>
2022-04-06 17:39:09 -0300

    Adiciona aula 10: Operadores Relacionais

--- CursoCSharp/Fundamentos/OperadoresRelacionais.cs
+++ CursoCSharp/Fundamentos/OperadoresRelacionais.cs
@@ -6,7 +6,17 @@ namespace CursoCSharp.Fundamentos {
     class OperadoresRelacionais {
         public static void Executar() {
 
+            Console.Write("Digite a nota: ");
+            double.TryParse(Console.ReadLine(), out double nota);
+            Convert.ToDouble(nota);
+            double notaDeCorte = 7.0;
 
+            Console.WriteLine("Nota inválida? {0}", nota < 10 && nota > 0);
+            Console.WriteLine("Perfeito? {0}", nota == 10.0);
+            Console.WriteLine("Tem como melhorar? {0}", nota != 10.0);
+            Console.WriteLine("Passou? {0}", nota >= notaDeCorte);
+            Console.WriteLine("Recuperação? {0}", nota < notaDeCorte);
+            Console.WriteLine("Reprovado? {0}", nota <= 3.0);
         }
     }
 }
