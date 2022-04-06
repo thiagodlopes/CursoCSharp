5626f221e7173b3abb777078b110e89ac9cd515b
Thiago Lopes <thiagodaniellopes99@gmail.com>
2022-04-06 18:21:32 -0300

    Adiciona aula de Operadores Unários

---
+++ CursoCSharp/Fundamentos/OperadoresUnarios.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos {
+    internal class OperadoresUnarios {
+        public static void Executar() {
+            var valorNegativo = -5;
+            var numero1 = 2;
+            var numero2 = 3;
+            var booleano = true;
+
+            Console.WriteLine(-valorNegativo);
+            Console.WriteLine(!booleano);
+
+            numero1++;
+            Console.WriteLine(numero1);
+            --numero1;
+            Console.WriteLine(numero1);
+
+            Console.WriteLine(numero1++ == --numero2); //Operador antes da varíavel tem prioridade e foi realizado antes da comparação, ao contrário do ++
+            Console.WriteLine($"{numero1} {numero2}");
+
+        }
+    }
+}
--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -19,6 +19,7 @@ namespace CursoCSharp {
                 {"Operadores Relacionais - Fundamentos", OperadoresRelacionais.Executar},
                 {"Operadores Lógicos - Fundamentos", OperadoresLogicos.Executar},
                 {"Operadores de Atribuição - Fundamentos", OperadoresAtribuicao.Executar},
+                {"Operadores Unários - Fundamentos", OperadoresUnarios.Executar},
 
 
             });
