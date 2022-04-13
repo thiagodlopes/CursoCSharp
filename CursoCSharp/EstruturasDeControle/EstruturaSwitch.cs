885fb0451a07712252ae10317d5e8839181b5d5f
Thiago Lopes <thiagodaniellopes99@gmail.com>
2022-04-13 13:39:07 -0300

    Adiciona Estrutura Switch

---
+++ CursoCSharp/EstruturasDeControle/EstruturaSwitch.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.EstruturasDeControle {
+    class EstruturaSwitch {
+        public static void Executar() {
+            Console.WriteLine("Avalie o atendimento com uma nota de 1 a 5)");
+            int.TryParse(Console.ReadLine(), out int nota);
+
+            switch (nota) {
+                case 0: 
+                    Console.WriteLine("Péssimo!");
+                    break;
+                case 1:
+                case 2:
+                    Console.WriteLine("Muito ruim, mané...");
+                    break;
+                case 3:
+                    Console.WriteLine("Ok");
+                    break;
+                case 4:
+                    Console.WriteLine("Bom");
+                    break;
+                case 5:
+                    Console.WriteLine("MONSTRO DO ATENDIMENTO!");
+                    break;
+                default:
+                    Console.WriteLine("A nota inserida não é válida...");
+                    break;
+            }
+            Console.WriteLine("Fim");
+        }
+    }
+}
