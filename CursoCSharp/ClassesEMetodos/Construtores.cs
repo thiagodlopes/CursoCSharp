4d2fca11116ea6b5e9689e12d91110f20a9a3349
Thiago Lopes <thiagodaniellopes99@gmail.com>
2022-04-14 11:00:10 -0300

    Adiciona Construtores (múltiplas classes no mesmo arquivo)

---
+++ CursoCSharp/ClassesEMetodos/Construtores.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.ClassesEMetodos {
+    
+    class Carro {
+        public string Modelo;
+        public string Fabricante;
+        public int Ano;
+
+        public Carro(string modelo, string fabricante, int ano) {
+            Modelo = modelo;
+            Fabricante = fabricante;
+            Ano = ano; 
+        }
+
+        public Carro() { //Habilita os construtores que não passam parâmetro direto
+
+        }
+    }
+    class Construtores {
+        public static void Executar() {
+            var carro = new Carro(); //Construtor padrão (Só pode ser usado quando não há construtor ou quando é definido de forma explícita)
+            carro.Fabricante = "FIAT";
+            carro.Modelo = "Uno";
+            carro.Ano = 2015;
+                Console.WriteLine($"{carro.Fabricante} {carro.Modelo} {carro.Ano}");
+
+            var carroConstrutor = new Carro("FIAT", "Marea", 2000);
+            Console.WriteLine($"{carroConstrutor.Modelo} {carroConstrutor.Fabricante} {carroConstrutor.Ano}");
+
+            //Outro jeito de criar
+            var carroOutroJeito = new Carro() {
+                Modelo = "Strada",
+                Fabricante = "FIAT",
+                Ano = 2002
+            };
+            Console.WriteLine($"{carroOutroJeito.Fabricante} {carroOutroJeito.Modelo} {carroOutroJeito.Ano}");
+
+        }
+    }
+}
--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -39,6 +39,7 @@ namespace CursoCSharp {
 
                 // Classes e Métodos
                 {"Membros - Classes e Métodos", Membros.Executar},
+                {"Construtores - Classes e Métodos", Construtores.Executar},
             });
 
             central.SelecionarEExecutar();
