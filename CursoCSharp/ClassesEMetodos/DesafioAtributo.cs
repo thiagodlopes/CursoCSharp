1173ce3258d507f4d7d44e1406bcf2bc0f02faa6
Thiago Lopes <thiagodaniellopes99@gmail.com>
2022-04-15 15:39:13 -0300

    Adiciona o desafio de acessar atributo

---
+++ CursoCSharp/ClassesEMetodos/DesafioAtributo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.ClassesEMetodos {
+    class DesafioAtributo {
+
+        int a = 10;
+
+        public static void Executar() {
+            //Acessar a variável a dentro do método Executar.
+            DesafioAtributo desafio = new DesafioAtributo();
+            Console.WriteLine(desafio.a);
+        }
+    }
+}
