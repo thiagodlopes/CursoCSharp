e50dedadc399969d19a985108c6750482f5139df
Thiago Lopes <thiagodaniellopes99@gmail.com>
2022-04-15 22:52:26 -0300

    Adiciona aula sobre Parâmetros Nomeados

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -45,6 +45,7 @@ namespace CursoCSharp {
                 {"Atributos Estáticos - Classes e Métodos", AtributosEstaticos.Executar},
                 {"Desafio Atributo - Classes e Métodos", DesafioAtributo.Executar},
                 {"Params - Classes e Métodos", Params.Executar},
+                {"Paramâmetros Nomeados - Classes e Métodos", ParametrosNomeados.Executar},
             });
 
             central.SelecionarEExecutar();
