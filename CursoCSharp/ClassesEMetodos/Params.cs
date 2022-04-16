c6df8173d185132afdeb5dd1e134a1c35f76bdd3
Thiago Lopes <thiagodaniellopes99@gmail.com>
2022-04-15 22:34:46 -0300

    Adiciona aula de Params

---
+++ CursoCSharp/ClassesEMetodos/Params.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.ClassesEMetodos {
+    class Params {
+        public static void Recepcionar(params string[] pessoas) {
+            foreach (var pessoa in pessoas) {
+                Console.WriteLine("Olá, {0}", pessoa);
+            }
+        }
+
+        public static void Executar() {
+            Recepcionar("Peter", "Fulano", "Beltrano");  
+        }
+    }
+}
